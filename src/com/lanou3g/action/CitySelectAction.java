be39ad3cbea7ab5aed81e224ba866fcf6f82ec29
廖健贵 <397420794@qq.com>
2017-10-27 17:19:40 +0800

    加注释

--- src/com/lanou3g/action/CitySelectAction.java
+++ src/com/lanou3g/action/CitySelectAction.java
@@ -15,6 +15,7 @@ import java.util.List;
  * Created by dllo on 17/10/27.
  */
 public class CitySelectAction extends ActionSupport {
+    //加注释
     private List<Area> areaList;
     //定义变量,用来接收选择的是哪个大区
     private int index;
